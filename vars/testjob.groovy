bdcec67c9e6f3ac18caa89c833c0965873438253
rameshz23 <rameshz23@yahoo.com>
2020-04-04 22:20:55 +0530

    Update testjob.groovy

--- vars/testjob.groovy
+++ vars/testjob.groovy
@@ -3,25 +3,23 @@ def call(body) {
     body.resolveStrategy = Closure.DELEGATE_FIRST
     body.delegate = config
     body()
+
     pipeline {
-    agent any
-    stages {
-        stage ('Example') {
-            steps {
-                // log.info 'Starting' 
-                script { 
-                    log.info 'Starting'
-                    log.warning 'Nothing to do!'
+            options {
+                buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
                 }
-            }
-        }
-    
-        
-        
-        
-        
-    
-        
-    }     
+        agent any
+
+	stages {
+	        stage ('Example') {
+            		steps {
+		                // log.info 'Starting' 
+		                script { 
+			                    log.info 'Starting'
+			                    log.warning 'Nothing to do!'
+		                }
+		            }
+	        }
+	}
 }
 }
