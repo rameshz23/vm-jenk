9d2d85d5ec662abcfd1f737b37738bc95573a6b9
ram <rameshz23@yahoo.com>
2020-04-04 19:17:11 +0530

    update vars folder

---
+++ vars/testjob.groovy
@@ -0,0 +1,14 @@
+pipeline {
+    agent none
+    stages {
+        stage ('Example') {
+            steps {
+                // log.info 'Starting' 
+                script { 
+                    log.info 'Starting'
+                    log.warning 'Nothing to do!'
+                }
+            }
+        }
+    }
+}
