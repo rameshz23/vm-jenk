757011422b4f50985429ee4a7fbe1ae881c895b1
rameshz23 <rameshz23@yahoo.com>
2020-04-04 19:53:16 +0530

    Update testjob.groovy

--- vars/testjob.groovy
+++ vars/testjob.groovy
@@ -1,3 +1,11 @@
+import hudson.model.*
+import hudson.EnvVars
+import groovy.json.JsonSlurperClassic
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import groovy.json.*
+import java.net.URL
+
 def call(body) {
     def config = [:]
     body.resolveStrategy = Closure.DELEGATE_FIRST
