4b41bcc448fa54970a300cd083b60c879f05573e
rameshz23 <rameshz23@yahoo.com>
2020-04-05 18:21:10 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -8,35 +8,25 @@ def call(body) {
             options {
                 buildDiscarder(logRotator(numToKeepStr: '5', artifactNumToKeepStr: '5'))
                 }
-	    parameters {
-
-                string(name: 'jobID', defaultValue: '' )
-                string(name: 'NEW_BUILDNUMBER', defaultValue: '' )
-                //string(name: 'dns_action', defaultValue: '' )
-                //file name:'input', description:'contains list of projects to be build'
-                }
         agent any
-
-	stages {
-	        stage ('Example') {
-            		steps {
-		                // log.info 'Starting' 
-		                script { 
-			                    log.info 'Starting'
-			                    log.warning 'Nothing to do!'
-		                }
-		            }
-	        }
-		stage ('application'){
-			steps {
-				script {
-					 echo "prepare environment "
-                            		 currentBuild.displayName = "$env.NEW_BUILDNUMBER"
-                                         common.prepareEnv()
-                                         cleanWs()
-				}
-			}
-		}
-	}
-}
+        stages {
+            stage('This one should be skipped') {
+                when {
+                    expression { false }
+                }
+                steps {
+                    echo "this should be skipped, but it does not ("
+                }
+            }
+            
+            stage('Back-end') {
+            agent {
+                docker { image 'maven:3-alpine' }
+            }
+            steps {
+                sh 'mvn --version'
+            }
+        }
+        }
+    }
 }
