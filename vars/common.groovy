fa4de900344814d52a7fe65d8b228fb357bb8d33
rameshz23 <rameshz23@yahoo.com>
2020-04-08 15:04:26 +0530

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -8,14 +8,18 @@ def prepareEnv(){
     env.jsonfilename = "$jobID" + "." + "json"
     jsonFile1 = "$WORKSPACE/input.json"
     def json1 = readJSON file: jsonFile1
+    env.os = json1['hostname']
+    env.hostname = json1['ip']
+    env.ip = json1['os']
+    
 
     // condition to verify input json fileExists
     sh """
-      //if [ -f inputfile.json ]; then
-         echo "file condition"
-         echo $env.hostname
+      if [ -f inputfile.json ]; then
+         echo env.os
+         echo env.hostname
       else
-         echo $env.hostname
+         echo no file found 
       fi
     """
  }
