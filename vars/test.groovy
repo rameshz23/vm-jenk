4d063e75c10eb373bc4bcaa382c4190a9cc46813
rameshz23 <rameshz23@yahoo.com>
2020-04-08 13:58:27 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -47,6 +47,13 @@ def call(body) {
 		stage ('application'){
 			steps {
 				script {
+					echo "current build number: ${currentBuild.number}"
+            echo "previous build number: ${currentBuild.previousBuild.getNumber()}"
+            def causes = currentBuild.rawBuild.getCauses()
+            echo "causes: ${causes}"
+            def rebuildCause0 = currentBuild.rawBuild.getCause(com.sonyericsson.rebuild.RebuildCause)
+            echo "rebuildCause0: ${rebuildCause0}"
+            echo "rebuild up number: ${rebuildCause0.getUpstreamBuild()}"
 					 echo "prepare environment "
                             		// currentBuild.displayName = "$env.NEW_BUILDNUMBER"
                                         // common.prepareEnv()
